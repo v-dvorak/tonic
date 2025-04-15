6ca63a1b79040251c1a06c04d54b2bc87485406e
v-dvorak <vojtech.dvorak11@seznam.cz>
2025-04-15 21:18:47 +0200

    Implement evaluation for the OLiMPiC dataset

---
+++ tonic/SERVal/olimpic/__main__.py
@@ -0,0 +1,123 @@
+import random
+from argparse import ArgumentParser
+from pathlib import Path
+from timeit import default_timer as timer
+
+from prettytable import PrettyTable, MARKDOWN
+from tqdm import tqdm
+
+from datasetup.olimpic import OLIMPIC_ENTRY_POINT
+from odtools.Download import (get_path_to_latest_version, update_models, OLA_TAG, NOTA_TAG)
+from odtools.Inference.ModelWrappers import YOLODetectionModelWrapper
+from .pipeline import process_single_olimpic_image
+from ..utils import compute_LMX_metrics
+from ...Linearization import LMXWrapper
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--count", type=int, default=100, help="Number of images to process")
+    parser.add_argument("-s", "--seed", type=int, default=42, help="Random seed for data shuffling")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
+    parser.add_argument("-o", "--output", type=Path, default=None, help="Store detailed results to a CSV file")
+    parser.add_argument("--raise_err", action="store_true", help="Raise exception if errors occur")
+
+    args = parser.parse_args()
+
+    # setup models
+    update_models()
+    notehead_detector = YOLODetectionModelWrapper(get_path_to_latest_version(NOTA_TAG))
+    staff_detector = YOLODetectionModelWrapper(get_path_to_latest_version(OLA_TAG))
+
+    # load images
+    images = sorted(list(OLIMPIC_ENTRY_POINT.rglob("*.png")))
+    mxlms = sorted(list(OLIMPIC_ENTRY_POINT.rglob("*.musicxml")))
+
+    if len(images) != len(mxlms):
+        raise ValueError("Numbers of annotations and images do not match.")
+
+    if args.count > len(mxlms):
+        print("Warning: Number of data specified is greater than the number of data available")
+        args.count = len(mxlms)
+
+    # retrieve specified count of images
+    data = list(zip(images, mxlms))
+    random.Random(args.seed).shuffle(data)
+    data = data[:args.count]
+
+    # setup logs
+    total_processed = 0
+    total_score = [0 for _ in range(4)]
+    error_log = ""
+    error_count = 0
+    table = PrettyTable(["ID", "standardized", "reduced", "melody", "contour"])
+    table.set_style(MARKDOWN)
+
+    start = timer()
+    for image_path, mxml_path in tqdm(data, desc="Processing", disable=args.verbose):
+        if args.verbose:
+            print(f">>> {mxml_path}")
+        try:
+            gt_lmx = LMXWrapper.from_complex_musicxml_file(mxml_path)
+
+            p_lmx = process_single_olimpic_image(
+                image_path,
+                staff_detector,
+                notehead_detector,
+                verbose=False,
+                visualize=False
+            )
+            p_lmx.canonicalize()
+
+            stand, red, mel, cont = compute_LMX_metrics(p_lmx, gt_lmx)
+
+            table.add_row([
+                image_path.stem,
+                f"{stand:.4f}",
+                f"{red:.4f}",
+                f"{mel:.4f}",
+                f"{cont:.4f}",
+            ])
+            if args.verbose:
+                print(f"Score: {stand}")
+
+            total_processed += 1
+            total_score[0] += stand
+            total_score[1] += red
+            total_score[2] += mel
+            total_score[3] += cont
+
+        except Exception as e:
+            if args.raise_err:
+                raise e
+
+            error_log += f"{mxml_path}: {e}\n"
+            error_count += 1
+            print(f"{mxml_path}: {e}")
+
+        if args.verbose:
+            print()
+
+    time_elapsed = timer() - start
+
+    if args.output:
+        args.output.mkdir(exist_ok=True, parents=True)
+        with open(args.output) as file:
+            file.write(table.to_csv(index=False))
+
+    print(f"Error log:\n{error_log}")
+    print()
+    print(f"Processed: {total_processed}")
+    print(f"Errors: {error_count}")
+    print()
+    print(f"Scores:")
+    print(f">>> Standardized: {total_score[0] / total_processed:.4f}")
+    print(f">>> Reduced: {total_score[1] / total_processed:.4f}")
+    print(f">>> Melody: {total_score[2] / total_processed:.4f}")
+    print(f">>> Contour: {total_score[3] / total_processed:.4f}")
+    print()
+    print("Time elapsed:", time_elapsed)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tonic/SERVal/olimpic/pipeline.py
@@ -0,0 +1,193 @@
+from pathlib import Path
+
+import cv2
+import math
+from PIL import Image
+
+from odtools import BoundingBox
+from odtools.Inference import InferenceJob, SplitSettings, run_multiple_prediction_jobs, IModelWrapper
+from tonic import NOTEHEAD_TYPE_TAG, NoteheadType, NodeName, Node, reconstruct_note_events, LMXWrapper, \
+    linearize_note_events_to_lmx
+from tonic import (preprocess_annots_for_reconstruction)
+from tonic import (refactor_measures_on_page)
+from tonic.Reconstruction.VizUtils import visualize_input_data
+
+
+def run_predictions_single_image(
+        image_path: Path,
+        staff_detector: IModelWrapper,
+        notehead_detector: IModelWrapper,
+        verbose: bool = False,
+        visualize: bool = False,
+) -> tuple[list[Node], list[Node], list[Node]]:
+    """
+    returns: measures, grand staffs, noteheads
+    """
+    # SETUP INFERENCE JOBS
+    # convert image to bw beforehand
+    # (color to bw conversion from cv2 does not work in this case)
+    loaded_image = Image.open(image_path)
+    bw_image = loaded_image.convert("L")
+
+    image_width, image_height = loaded_image.size
+
+    # staff
+    staff_job = InferenceJob(
+        image=bw_image,
+        model_wrapper=staff_detector,
+        # retrieve only measures and grand staffs
+        wanted_ids=[1, 4]
+    )
+
+    # noteheads
+    notehead_job = InferenceJob(
+        image=cv2.imread(image_path),
+        model_wrapper=notehead_detector,
+        # retrieve only full and empty noteheads
+        wanted_ids=[0, 1],
+        split_settings=SplitSettings(
+            width=image_height,
+            height=image_height,
+            overlap_ratio=0.10,
+            iou_threshold=0.25,
+            edge_offset_ratio=0.04
+        )
+    )
+
+    # RUN INFERENCE JOBS
+    combined = run_multiple_prediction_jobs(
+        [
+            staff_job,
+            notehead_job,
+        ],
+        verbose=False
+    )
+
+    if verbose:
+        print(f"Class names: {', '.join(combined.class_names)}")
+        print()
+
+    # INITIALIZE GRAPH
+    prepro_def = [
+        (
+            NodeName.MEASURE, combined.annotations[0]
+        ),
+        (
+            NodeName.GRAND_STAFF, combined.annotations[1]
+        ),
+        (
+            NodeName.NOTEHEAD, [NOTEHEAD_TYPE_TAG],
+            [
+                (combined.annotations[2], [NoteheadType.FULL]),
+                (combined.annotations[3], [NoteheadType.HALF])
+            ]
+        ),
+    ]
+
+    measures, grand_staffs, noteheads = preprocess_annots_for_reconstruction(prepro_def)
+
+    if visualize:
+        visualize_input_data(
+            image_path,
+            measures,
+            notehead_full=noteheads,
+            notehead_half=grand_staffs
+        )
+
+    return measures, grand_staffs, noteheads
+
+
+def refine_detections(
+        image_path: Path,
+        measures: list[Node],
+        grand_staffs: list[Node],
+        noteheads: list[Node],
+        vertical_offset_factor: float,
+        verbose: bool = False,
+        visualize: bool = False,
+) -> tuple[list[Node], list[Node], list[Node]]:
+    """
+    returns: measures, grand_staffs, noteheads
+    """
+    image_width, image_height = Image.open(image_path).size
+
+    refactor_measures_on_page(
+        measures,
+        image_path,
+        verbose=verbose,
+        visualize=False
+    )
+
+    valid_box = BoundingBox(
+        0,
+        math.ceil(image_height * vertical_offset_factor),
+        image_width,
+        math.ceil(image_height * (1 - vertical_offset_factor))
+    )
+
+    measures: list[Node]
+    measures = [m for m in measures if m.annot.bbox.is_fully_inside(valid_box)]
+    grand_staffs = [gs for gs in grand_staffs if gs.annot.bbox.is_fully_inside(valid_box)]
+
+    if visualize:
+        visualize_input_data(
+            image_path,
+            measures,
+            notehead_full=noteheads,
+            notehead_half=grand_staffs
+        )
+
+    return measures, grand_staffs, noteheads
+
+
+def detections_to_lmx_wrapper(
+        measures: list[Node],
+        grand_staffs: list[Node],
+        noteheads: list[Node],
+        verbose: bool = False,
+) -> LMXWrapper:
+    events = reconstruct_note_events(
+        measures,
+        grand_staffs,
+        noteheads,
+        ual_factor=1.8,
+        neiou_threshold=0.4,
+        verbose=verbose
+    )
+
+    return linearize_note_events_to_lmx(events)
+
+
+def process_single_olimpic_image(
+        image_path: Path,
+        staff_detector: IModelWrapper,
+        notehead_detector: IModelWrapper,
+        vertical_offset_factor: float = 0.2,
+        verbose: bool = False,
+        visualize: bool = False,
+) -> LMXWrapper:
+    """
+    Predicts noteheads, staff and grand staff for a single image from the OLiMPiC dataset.
+
+    :param image_path: Path to an image
+    :param staff_detector: wrapped model for staff and grand staff detection
+    :param notehead_detector: wrapped model for notehead detection
+    :param vertical_offset_factor: how much to offset safe zone from image edges relative to image height
+    :param verbose: make script verbose
+    :param visualize: visualize inference
+    """
+    measures, grand_staffs, noteheads = run_predictions_single_image(
+        image_path,
+        staff_detector,
+        notehead_detector,
+        verbose=verbose,
+        visualize=visualize
+    )
+    measures, grand_staffs, noteheads = refine_detections(
+        image_path,
+        measures, grand_staffs, noteheads,
+        vertical_offset_factor=vertical_offset_factor,
+        verbose=verbose,
+        visualize=visualize
+    )
+    return detections_to_lmx_wrapper(measures, grand_staffs, noteheads)
